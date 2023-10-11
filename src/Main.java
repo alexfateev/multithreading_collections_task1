d04fb5e2bf235a490987231a4bc9158c0531062e
alexfateev <49758927+alexfateev@users.noreply.github.com>
2023-10-11 15:56:43 +0500

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -10,9 +10,7 @@ public class Main {
     static BlockingQueue<String> list1 = new ArrayBlockingQueue<>(LIST_CAPACITY);
     static BlockingQueue<String> list2 = new ArrayBlockingQueue<>(LIST_CAPACITY);
     static BlockingQueue<String> list3 = new ArrayBlockingQueue<>(LIST_CAPACITY);
-    static String textMaxA = "";
-    static String textMaxB;
-    static String textMaxC;
+    static Thread generateText;
 
     public static String generateText(String letters, int length) {
         Random random = new Random();
@@ -23,12 +21,33 @@ public class Main {
         return text.toString();
     }
 
-    public static long countLetter(String text, char ch) {
-        return text.chars().filter(c -> c == ch).count();
+    public static int showOccurrenceLetter(BlockingQueue<String> queue, char letter) {
+        String text;
+        int max = 0;
+        int count;
+        try {
+            while (generateText.isAlive()) {
+                text = queue.take();
+                count = (int) text.chars().filter(c -> c == letter).count();
+                if (count > max) {
+                    max = count;
+                }
+            }
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        return max;
+    }
+
+    static Thread getThread(BlockingQueue<String> queue, char letter) {
+        return new Thread(() -> {
+            int max = showOccurrenceLetter(queue, letter);
+            System.out.println("Длина строки содержащая больше всего " + letter + ": " + max);
+        });
     }
 
     public static void main(String[] args) throws InterruptedException {
-        Thread threadGenerate = new Thread(() -> {
+        generateText = new Thread(() -> {
             for (int i = 0; i < COUNT; i++) {
                 String text = generateText("abc", 100_000);
                 try {
@@ -41,59 +60,19 @@ public class Main {
             }
         });
 
-        Thread thread1 = new Thread(() -> {
-            try {
-                for (int i = 0; i < COUNT; i++) {
-                    String str = list1.take();
-                    if (countLetter(str, 'a') > countLetter(textMaxA, 'a')) {
-                        textMaxA = str;
-                    }
-                }
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-        });
+        Thread thread1 = getThread(list1, 'a');
+        Thread thread2 = getThread(list2, 'b');
+        Thread thread3 = getThread(list3, 'c');
 
-        Thread thread2 = new Thread(() -> {
-            try {
-                for (int i = 0; i < COUNT; i++) {
-                    String str = list2.take();
-                    if (countLetter(str, 'b') > countLetter(textMaxA, 'b')) {
-                        textMaxA = str;
-                    }
-                }
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-        });
 
-        Thread thread3 = new Thread(() -> {
-            try {
-                for (int i = 0; i < COUNT; i++) {
-                    String str = list3.take();
-                    if (countLetter(str, 'c') > countLetter(textMaxA, 'c')) {
-                        textMaxA = str;
-                    }
-                }
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-        });
-
-        threadGenerate.start();
+        generateText.start();
         thread1.start();
         thread2.start();
         thread3.start();
 
-        threadGenerate.join();
+        generateText.join();
         thread1.join();
         thread2.join();
         thread3.join();
-
-        System.out.println("Строка содержащая баольше всего а: " + countLetter(textMaxA, 'a'));
-        System.out.println("Строка содержащая баольше всего b: " + countLetter(textMaxA, 'b'));
-        System.out.println("Строка содержащая баольше всего c: " + countLetter(textMaxA, 'c'));
-
-
     }
 }
